9d76497341cf84af6c226afbee40ff2db6e22389
Darshan Lukhi <darshan.lukhi@crestdatasys.com>
2024-03-06 15:12:58 +0530

    Anonymous Commit

--- data.go
+++ data.go
@@ -3,7 +3,6 @@ package main
 import (
 	_ "embed"
 	"encoding/csv"
-	"encoding/json"
 	"io"
 	"strconv"
 	"strings"
@@ -17,41 +16,6 @@ var csvAuthors string
 
 //go:embed data/institutions.json
 var jsonInstitutions []byte
-
-func readAuthors() ([]Author, error) {
-	r := csv.NewReader(strings.NewReader(csvAuthors))
-	results := make([]Author, 0)
-	// skip the title row
-	_, err := r.Read()
-	if err == io.EOF {
-		return results, nil
-	}
-	if err != nil {
-		return nil, err
-	}
-
-	for {
-		record, err := r.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			return nil, err
-		}
-
-		id, err := strconv.ParseInt(record[0], 10, 32)
-		if err != nil {
-			return nil, err
-		}
-		results = append(results, Author{
-			ID:        int(id),
-			FirstName: record[1],
-			LastName:  record[2],
-		})
-	}
-
-	return results, nil
-}
 
 func readArticles() ([]Article, error) {
 	r := csv.NewReader(strings.NewReader(csvArticles))
@@ -90,13 +54,4 @@ func readArticles() ([]Article, error) {
 	}
 
 	return results, nil
-}
-
-func readInstitutions() ([]Institution, error) {
-	var institutions []Institution
-	if err := json.Unmarshal(jsonInstitutions, &institutions); err != nil {
-		return nil, err
-	}
-
-	return institutions, nil
 }
